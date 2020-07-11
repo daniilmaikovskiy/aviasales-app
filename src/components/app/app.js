c25715c822a4bf23a1c6a2e8568afab2cd9b82bc
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-11 14:34:51 +0300

    init

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './app.module.scss';
+
+export default function App() {
+  return (
+    <div className={classes.wrapper}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
