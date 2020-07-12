7e5597b7b281951f20fd5089a8895f4d396a4e83
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-12 12:33:01 +0300

    added mobile version

--- src/components/transfers-menu/transfers-menu.js
+++ src/components/transfers-menu/transfers-menu.js
@@ -5,12 +5,14 @@ import CustomCheckbox from '../custom-checkbox';
 export default function TransfersMenu() {
   return (
     <div className={classes.wrapper}>
-      <div className={`${classes.title} ${classes.row}`}>Количество пересадок</div>
-      <CustomCheckbox className={classes.row} text="Все" />
-      <CustomCheckbox className={classes.row} text="Без пересадок" />
-      <CustomCheckbox className={classes.row} text="1 пересадка" />
-      <CustomCheckbox className={classes.row} text="2 пересадки" />
-      <CustomCheckbox className={classes.row} text="3 пересадки" />
+      <div className={classes.menu}>
+        <div className={`${classes.title} ${classes.row}`}>Количество пересадок</div>
+        <CustomCheckbox className={classes.row} text="Все" />
+        <CustomCheckbox className={classes.row} text="Без пересадок" />
+        <CustomCheckbox className={classes.row} text="1 пересадка" />
+        <CustomCheckbox className={classes.row} text="2 пересадки" />
+        <CustomCheckbox className={classes.row} text="3 пересадки" />
+      </div>
     </div>
   );
 }
