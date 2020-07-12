1f25007ba43ee93cbd0647c69dd4cc2ab443143e
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-12 12:01:48 +0300

    Updates

---
+++ precache-manifest.82aea908f8d0148017db2406339379f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e2e0e411206619ac6528e22a4e280f",
+    "url": "/aviasales-app/index.html"
+  },
+  {
+    "revision": "44ec91bee0b035a42e4a",
+    "url": "/aviasales-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "57e868506c28515f454f",
+    "url": "/aviasales-app/static/css/main.4df96434.chunk.css"
+  },
+  {
+    "revision": "44ec91bee0b035a42e4a",
+    "url": "/aviasales-app/static/js/2.747d4451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aviasales-app/static/js/2.747d4451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e868506c28515f454f",
+    "url": "/aviasales-app/static/js/main.3b12e64e.chunk.js"
+  },
+  {
+    "revision": "57f25e0e36f3e44f8d3c",
+    "url": "/aviasales-app/static/js/runtime-main.1aaf4800.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/aviasales-app/precache-manifest.82aea908f8d0148017db2406339379f2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aviasales-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
