ced49df46e5826b4df30641c66e131c71283bda1
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-13 17:41:38 +0300

    added redux-thunk and redux devtools

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 import reducer from './reducer/reducer';
 import App from './components/app';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <Provider store={store}>
